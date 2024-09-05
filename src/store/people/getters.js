baa533ecbb32f6582e5285ca621f28517f9c5ae6
Luiz Kim <luiz.kim@controleonline.com>
2024-09-05 15:30:14 -0300

    Update

--- src/store/people/getters.js
+++ src/store/people/getters.js
@@ -2,4 +2,3 @@ export const company        = state => state.company;
 export const currentCompany = state => state.currentCompany;
 export const defaultCompany = state => state.defaultCompany;
 export const companies      = state => state.companies;
-export const currentPerson = state => state.item;
