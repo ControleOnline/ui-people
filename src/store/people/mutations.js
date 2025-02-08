6e86b723884963f8241f81cc29396054899d0a43
Luiz Kim <luiz.kim@controleonline.com>
2025-02-08 13:50:00 -0300

    Update

--- src/store/people/mutations.js
+++ src/store/people/mutations.js
@@ -1,20 +1,15 @@
 import * as types from "./mutation_types";
 
 export default {
-  [types.SET_COMPANIES](state, payload) {
-    if (!payload?.companies) Object.assign(state, { companies: payload });
-    return { ...state, companies: payload?.companies || payload };
+  [types.SET_COMPANIES](state, companies) {
+    state.companies = companies;
   },
 
-  [types.SET_CURRENT_COMPANY](state, payload) {
-    if (!payload?.currentCompany)
-      Object.assign(state, { currentCompany: payload });
-    return { ...state, currentCompany: payload?.currentCompany || payload };
+  [types.SET_CURRENT_COMPANY](state, currentCompany) {
+    state.currentCompany = currentCompany;
   },
 
-  [types.SET_DEFAULT_COMPANY](state, payload) {
-    if (!payload?.defaultCompany)
-      Object.assign(state, { defaultCompany: payload });
-    return { ...state, defaultCompany: payload?.defaultCompany || payload };
+  [types.SET_DEFAULT_COMPANY](state, defaultCompany) {
+    state.defaultCompany = defaultCompany;
   },
 };
