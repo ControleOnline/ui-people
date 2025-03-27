6a34dbb12b07f68f6e77ff7c63ae6479d32592a1
Luiz Kim <luiz.kim@controleonline.com>
2025-03-27 13:16:29 -0300

    Update

--- src/vue/pages/Company.vue
+++ src/vue/pages/Company.vue
@@ -1,24 +1,25 @@
 <template>
-  <People :context="context" :myCompany="$auth.user" v-if="context"  />
+  <People :context="context" :myCompany="user" v-if="context" />
 </template>
 
 <script>
 import People from "@controleonline/ui-people/src/vue/components/People/List.vue";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   components: {
     People,
   },
   computed: {
-
+    ...mapGetters({
+      user: "auth/user",
+    }),
   },
   data() {
     return {
       context: "company",
     };
   },
-  created() {
-
-  },
+  created() {},
 };
 </script>
