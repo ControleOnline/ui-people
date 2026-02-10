a7d10d9b7a3d6b216061b74a487d305fda942a06
Luiz Kim <luiz.kim@controleonline.com>
2026-02-10 14:25:56 -0300

    Change Routes

--- src/react/router/routes.js
+++ src/react/router/routes.js
@@ -1,24 +1,10 @@
 import Profile from '@controleonline/ui-people/src/react/pages/Profile';
-import DefaultLayout from '@controleonline/ui-layout/src/react/layouts/DefaultLayout';
-// import DefaultLayout from '@controleonline/ui-layout/src/react/layouts/DefaultLayout';
 
-import React from 'react';
-
-const WrappedProfile = ({navigation, route}) => (
-  <DefaultLayout navigation={navigation} route={route}>
-    <Profile navigation={navigation} route={route} />
-  </DefaultLayout>
-);
-// const WrappedProfile = ({navigation, route}) => (
-//   <DefaultLayout navigation={navigation} route={route}>
-//     <Profile navigation={navigation} route={route} />
-//   </DefaultLayout>
-// );
 
 const peopleRoutes = [
   {
     name: 'ProfilePage',
-    component: WrappedProfile,
+    component: Profile,
     options: {
       headerShown: true,
       title: 'Perfil',
