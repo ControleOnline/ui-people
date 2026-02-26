9d342fdedfbdd70d841692e5a36c488a021015d2
Luiz Kim <luiz.kim@controleonline.com>
2026-02-26 19:10:30 -0300

    Reverting

--- src/react/router/routes.js
+++ src/react/router/routes.js
@@ -1,25 +1,16 @@
 import Profile from '@controleonline/ui-people/src/react/pages/Profile';
-import CrmLayout from '@controleonline/ui-layout/src/react/layouts/CrmLayout';
 
-import React from 'react';
-
-const WrappedProfile = ({ navigation, route }) => (
-  <CrmLayout navigation={navigation} route={route} showCompanyFilter={false}>
-    <Profile navigation={navigation} route={route} />
-  </CrmLayout>
-);
 
 const peopleRoutes = [
   {
     name: 'ProfilePage',
-    component: WrappedProfile,
+    component: Profile,
     options: {
       headerShown: true,
+      headerBackVisible: true,
       title: 'Perfil',
-      headerLeft: () => null,
-      headerBackButtonMenuEnabled: false,
     },
-    initialParams: { store: 'auth' },
+    initialParams: {store: 'auth'},
   },
 ];
 
