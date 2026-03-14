b906300b96f2440e0e744748207eb8d3ab5d8a22
Luiz Kim <luiz.kim@controleonline.com>
2026-03-13 22:05:04 -0300

    sellers-client

---
+++ src/store/people_link/index.js
@@ -0,0 +1,27 @@
+import * as actions from '@controleonline/ui-default/src/store/default/actions';
+import * as getters from '@controleonline/ui-default/src/store/default/getters';
+import mutations from '@controleonline/ui-default/src/store/default/mutations';
+import Formatter from '@controleonline/ui-common/src/utils/formatter.js';
+
+export default {
+  namespaced: true,
+  state: {
+    item: {},
+    items: null,
+    resourceEndpoint: 'people_links',
+    isLoading: false,
+    error: '',
+    totalItems: 0,
+    messages: [],
+    message: {},
+    filters: {},
+    currentCompany: {},
+    defaultCompany: {},
+    companies: [],
+    columns: [
+    ],
+  },
+  actions,
+  getters,
+  mutations,
+};
