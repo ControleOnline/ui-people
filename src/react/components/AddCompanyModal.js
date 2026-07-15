e58d098ab9923e46c29b107d258268f7ae52f659
Luiz Kim <luizkim@users.noreply.github.com>
2026-07-15 16:18:15 -0300

    Use shared ui-common AnimatedModal

--- src/react/components/AddCompanyModal.js
+++ src/react/components/AddCompanyModal.js
@@ -11,7 +11,7 @@ import {
 import { Picker } from '@react-native-picker/picker';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import AnimatedModal from '@controleonline/ui-crm/src/react/components/AnimatedModal';
+import AnimatedModal from '@controleonline/ui-common/src/react/components/AnimatedModal';
 import { useMessage } from '@controleonline/ui-common/src/react/components/MessageService';
 import {
   formatDisplayUppercase,
